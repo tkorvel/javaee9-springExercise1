f42704b5ebc39aaa89be9aae407b83f864a1f518
tkorvel <korveltanel@gmail.com>
2022-01-28 20:53:20 +0200

    [init] - project initialization

---
+++ src/main/java/com/sda/javaee9springex1/Javaee9SpringEx1Application.java
@@ -0,0 +1,13 @@
+package com.sda.javaee9springex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaee9SpringEx1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javaee9SpringEx1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/javaee9springex1/Javaee9SpringEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.javaee9springex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaee9SpringEx1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
