305d33adc5296dd1303dd11ac7b8b21ad4df2fc3
tkorvel <korveltanel@gmail.com>
2022-01-29 00:22:02 +0200

    [change] - exercises with controller

--- src/main/java/com/sda/javaee9springex1/controller/FirstController.java
+++ src/main/java/com/sda/javaee9springex1/controller/FirstController.java
@@ -1,5 +1,7 @@
 package com.sda.javaee9springex1.controller;
 
+import lombok.Data;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,20 @@ public class FirstController {
     public String firstPage() {
         log.info("firstPage() method called!!!");
         // looking for html page inside resources/templates
+        // Spring please use page: resources/templates/home-page.html
         return "home-page";
+    }
+
+    @GetMapping("/my-second-page")
+    public String secondPage() {
+        log.info("secondPage() method called!!!");
+        //looking for html page inside resources/templates
+        return "second-page";
+    }
+
+    @GetMapping("/third-page")
+    public String thirdPage() {
+        log.info("thirdPage() method was called!!!");
+        return "pages/third-page";
     }
 }
---
+++ src/main/java/com/sda/javaee9springex1/controller/MyRunnable.java
@@ -0,0 +1,4 @@
+package com.sda.javaee9springex1.controller;
+
+public interface MyRunnable extends Runnable{
+}
